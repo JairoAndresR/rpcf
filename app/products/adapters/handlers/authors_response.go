d321883bc93cbb3ce820ccf1f9c71821a82b9231
cristianchaparroa <cristianchaparroa@gmail.com>
2020-12-10 19:36:23 -0500

    fix: correct the response mapping for authors

--- app/products/adapters/handlers/authors_response.go
+++ app/products/adapters/handlers/authors_response.go
@@ -23,6 +23,10 @@ func NewAuthorListResponse(list []*products.Author) *AuthorListResponse {
 
 	authors := make([]*AuthorResponse, 0)
 
+	for _, a := range list {
+		authors = append(authors,NewAuthorsResponse(a) )
+	}
+
 	return &AuthorListResponse{
 		Authors: authors,
 		Total:   len(authors), // TODO: change this value from the query result
