342730f3bb9f60c9e30e6b18e99c0f407292866c
cristianchaparroa <cristianchaparroa@gmail.com>
2020-12-12 23:40:54 -0500

    fix: remove the env variables from .env file

--- main.go
+++ main.go
@@ -1,18 +1,10 @@
 package main
 
 import (
-	"github.com/joho/godotenv"
-	log "github.com/sirupsen/logrus"
 	"rpcf/app/delivery/rest"
 )
 
 func main() {
-
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
 	s := rest.NewServer()
 	s.Run()
 }
