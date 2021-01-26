ea83eb53f9b9378ec8f069356a10bdfc7c9de6ed
cristianchaparroa <cristian.chaparro@commure.com>
2021-01-25 21:50:35 -0500

    fix: edit comment in authorDefinitionReader

--- products/ports/author_definition_reader.go
+++ products/ports/author_definition_reader.go
@@ -3,6 +3,6 @@ package ports
 import "rpcf/products"
 
 type AuthorDefinitionReader interface {
-	// GetAuthorDefinition retrieve the only author definition
+	// GetAuthorDefinition retrieve the author definition
 	GetAuthorDefinition() (*products.AuthorDefinition, error)
 }
