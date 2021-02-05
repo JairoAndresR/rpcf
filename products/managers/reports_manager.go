e23746dba6f1b5eb9b08b36d5438c3d8f0919ab6
cristianchaparroa <cristianchaparroa@gmail.com>
2021-02-05 12:46:21 -0500

    add the words frequency method in reports manager

--- products/managers/reports_manager.go
+++ products/managers/reports_manager.go
@@ -1,9 +1,11 @@
 package managers
 
 import (
+	analysisPorts "rpcf/analysis/ports"
 	"rpcf/core"
 	"rpcf/products"
 	"rpcf/products/ports"
+	"strings"
 )
 
 func init() {
@@ -12,17 +14,40 @@ func init() {
 }
 
 type ReportsManager struct {
-	reader ports.ReportsReader
+	reader           ports.ReportsReader
+	productReader    ports.ProductReader
+	frequencyCounter analysisPorts.WordFrequencyCounter
 }
 
-func newReportsManager(r ports.ReportsReader) ports.ReportsManager {
-	return &ReportsManager{reader: r}
+func newReportsManager(r ports.ReportsReader, pr ports.ProductReader) ports.ReportsManager {
+	return &ReportsManager{
+		reader:        r,
+		productReader: pr,
+	}
 }
 
 func (m *ReportsManager) CountAll(filters map[string]string, group string) ([]*products.Report, error) {
 	return m.reader.CountAllBy(filters, group)
 }
 
-func (m *ReportsManager) WordCloud(grupLACCode string) ([]*products.Report, error) {
-	panic("implement me")
+func (m *ReportsManager) WordsFrequency(filters map[string]string) ([]*products.Report, error) {
+	ps, err := m.productReader.GetAll(filters)
+
+	if err != nil {
+		return nil, err
+	}
+	var textBuilder strings.Builder
+	for _, p := range ps {
+		textBuilder.WriteString(p.Title)
+	}
+	text := textBuilder.String()
+	frequencies := m.frequencyCounter.Count(text)
+
+	reports := make([]*products.Report, 0)
+	for text, count := range frequencies {
+		report := products.NewReport(text, int64(count))
+		reports = append(reports, report)
+	}
+
+	return reports, nil
 }
--- products/ports/reports_manager.go
+++ products/ports/reports_manager.go
@@ -3,8 +3,7 @@ package ports
 import "rpcf/products"
 
 type ReportsManager interface {
-
 	CountAll(filters map[string]string, group string) ([]*products.Report, error)
 
-	WordCloud(grupLACCode string) ([]*products.Report, error)
+	WordsFrequency(filters map[string]string) ([]*products.Report, error)
 }
