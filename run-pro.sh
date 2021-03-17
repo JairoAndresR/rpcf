b45b0157e0b9719c721f8c81e0801b3b6bd43cb9
Jairo Lavado <jlavadoh@correo.udistrital.edu.co>
2021-03-17 12:07:28 -0500

    Update run-pro.sh

--- run-pro.sh
+++ run-pro.sh
@@ -1,5 +1,6 @@
 cd  /opt/app/
-docker-compose stop app swagger
-docker-compose up --force-recreate --build --remove-orphans -d app swagger
 chmod +x run.sh
 sh run.sh
+docker-compose stop app swagger
+docker-compose up --force-recreate --build --remove-orphans -d app swagger
+
